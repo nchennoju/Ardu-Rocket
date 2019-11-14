1714c02147f518fb84f3f7770a24eae955cad731
Nitish Chennoju <nchennoju@gmail.com>
2019-11-14 15:59:07 -0700

    Add files via upload

---
+++ RocketFULL/RocketFULL.ino
@@ -0,0 +1,234 @@
+#include <Wire.h>
+
+#include <Servo.h>
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+//MY VARIABLES
+const int timeDelay = 5;
+
+boolean once = true;
+
+Servo servoX1;
+Servo servoX2;
+Servo servoY1;
+Servo servoY2;
+
+int rollAmount;
+int pitchAmount;
+int yawAmount;
+
+int offset[4] = {-9, 7, -7, 0}; //Pins 9, 10, 11, 12
+
+void setup() {
+
+    servoX1.attach(12);
+    servoX2.attach(11);
+    servoY1.attach(10);
+    servoY2.attach(9);
+    
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    //5 SECOND DELAY
+    for(int i = timeDelay; i >= 1; i--){
+      Serial.print(i);
+      Serial.println("...");
+      delay(1000);
+    }
+
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
+
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        /*if(once){
+          gyroOffset[0] = rollAmount;
+          gyroOffset[1] = pitchAmount);
+          once = false;
+          Serial.println("GYRO OFFSETS SET...");
+          delay(1000);
+        }*/
+        
+        
+        //180 - (rollAmount + 90 + pitchAmount)
+
+        servoX1.write((rollAmount * 0.8 + 90) + offset[3] - yawAmount);
+        servoX2.write((90 - 0.8 * rollAmount) + offset[2] - yawAmount);
+
+        servoY1.write((pitchAmount * 0.8 + 90) + offset[1] - yawAmount);
+        servoY2.write((90 - 0.8 * pitchAmount) + offset[0] - yawAmount);
+
+        
+    }
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_QUATERNION
+            // display quaternion values in easy matrix form: w x y z
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+        #endif
+
+        #ifdef OUTPUT_READABLE_EULER
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
+        #endif
+
+//IMPORTANT! THIS IS WHERE YPR IS PRINTED
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+
+            if((int)((ypr[0] * 180/M_PI)) < -15){
+              yawAmount = -15;
+            }else if((int)(ypr[0] * 180/M_PI) > 15){
+              yawAmount = 15;
+            }else{
+              yawAmount = (int)(ypr[0] * 180/M_PI);
+            } 
+            
+            if((int)((ypr[1] * 180/M_PI)) < -19){
+              pitchAmount = -19;
+            }else if((int)(ypr[1] * 180/M_PI) > 19){
+              pitchAmount = 19;
+            }else{
+              pitchAmount = (int)(ypr[1] * 180/M_PI);
+            }
+
+            if((int)((ypr[2] * 180/M_PI)) < -19){
+              rollAmount = -19;
+            }else if((int)(ypr[2] * 180/M_PI) > 19){
+              rollAmount = 19;
+            }else{
+              rollAmount = (int)(ypr[2] * 180/M_PI);
+            } 
+        #endif
+
+     }
+}
---
+++ RocketResetPos/RocketResetPos.ino
@@ -0,0 +1,41 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo ms1;
+Servo ms2;
+Servo ms3;
+Servo ms4;
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  ms1.attach(9);
+  ms2.attach(10);
+  ms3.attach(11); //done
+  ms4.attach(12); //done
+}
+
+void loop() {
+  ms1.write(81);
+  ms2.write(97);
+  ms3.write(83);
+  ms4.write(90);
+  /*for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }*/
+}
+
